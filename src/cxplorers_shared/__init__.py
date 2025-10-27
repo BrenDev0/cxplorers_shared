24caf0ec3404e2be6e6497e35c1aa4e4ac309089
BrenDev0 <lahey1991@gmail.com>
2025-10-27 14:08:06 -0600

    version mismatch

--- src/cxplorers_shared/__init__.py
+++ src/cxplorers_shared/__init__.py
@@ -1,5 +1,5 @@
 from cxplorers_shared.services.encryption_service import EncryptionService
 from cxplorers_shared.utils.http_client import SecureHTTPClient
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 __all__ = ["EncryptionService", "SecureHTTPClient"]
