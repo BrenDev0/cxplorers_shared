5b6002261409380616e5547111cbf24fd8282b45
BrenDev0 <lahey1991@gmail.com>
2025-10-27 13:19:10 -0600

    WIP: package

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+from src.services.encryption_service import EncryptionService
+from src.utils.http_client import SecureHTTPClient
+
+__version__ = "0.1.0"
+__all__ = ["EncryptionService", "SecureHTTPClient"]
