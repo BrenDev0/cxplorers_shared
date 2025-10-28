f4405a034733858e6e7325ffb3f9f46c8ae81460
BrenDev0 <lahey1991@gmail.com>
2025-10-28 17:17:36 -0600

    data repo base

--- src/cxplorers_shared/__init__.py
+++ src/cxplorers_shared/__init__.py
@@ -1,5 +1,6 @@
 from cxplorers_shared.services.encryption_service import EncryptionService
 from cxplorers_shared.utils.http_client import SecureHTTPClient
+from cxplorers_shared.domain.repositories.data_repository_base import DataRepository
 
-__version__ = "0.1.5"
-__all__ = ["EncryptionService", "SecureHTTPClient"]
+__version__ = "0.1.6"
+__all__ = ["EncryptionService", "SecureHTTPClient", "DataRepository"]
