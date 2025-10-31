38b7b45aa258192862a9a7b05c83933d391012f1
BrenDev0 <lahey1991@gmail.com>
2025-10-31 09:36:59 -0600

    repoexceptions

--- src/cxplorers_shared/utils/decorators/error_handler.py
+++ src/cxplorers_shared/utils/decorators/error_handler.py
@@ -1,10 +1,11 @@
 from functools import wraps
 import logging
 from typing import Callable, Any
+from cxplorers_shared.domain.exceptions.repository import RepositoryError
 
 logger = logging.getLogger("Error_handler")
 
-def service_error_handler(module: str) -> Callable:
+def service_error_handler(module: str, type: str = "python") -> Callable:
     def decorator(func: Callable) -> Callable:
         @wraps(func) 
         def wrapper(*args: Any, **kwargs: Any) -> Any:
@@ -18,6 +19,12 @@ def service_error_handler(module: str) -> Callable:
                     exc_info=True,
                     stack_info=2
                 )
-                raise  
+
+                match type:
+                    case "repository":
+                        raise RepositoryError()
+                    case _:
+                        raise
+                  
         return wrapper
     return decorator
